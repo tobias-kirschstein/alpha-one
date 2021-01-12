054f23d93382b014c922ed31db55336d9c948651
Tobias Kirschstein <tobias.kirschstein@tum.de>
2021-01-13 00:54:21 +0100

    Update Policy Gradient Baseline notebook

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='alpha_one',
       author='Tobias Kirschstein',
       author_email='kirschto@in.tum.de',
       packages=['alpha_one'],
-      install_requires=['envyaml', 'trueskill', 'open_spiel', 'ray', 'numpy'],
+      install_requires=['envyaml', 'trueskill', 'open_spiel', 'ray', 'numpy', 'tqdm'],
       zip_safe=False)
